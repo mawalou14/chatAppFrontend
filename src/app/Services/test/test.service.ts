f348235c7c4ec7ca1aca79ca74eb748417b63850
mawalou14 <awaloum213@gmail.com>
2023-12-25 00:28:46 +0100

    create getTest fx

--- src/app/Services/test/test.service.ts
+++ src/app/Services/test/test.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export interface Test {
   title: string;
@@ -12,5 +13,7 @@ export class TestService {
 
   constructor(private http: HttpClient) { }
 
- 
+ getTest(): Observable<Test> {
+  return this.http.get<Test>('api');
+ }
 }
