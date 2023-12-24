9a1b5972e863818730bec8a56a413d6a12a2a09d
mawalou14 <awaloum213@gmail.com>
2023-12-25 00:42:07 +0100

    add environment

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  baseUrl: 'localhost:3000/api'
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  baseUrl: 'localhost:3000/api'
+};
